7df3efcec40f2a09da247a2199911861d3519202
0x41ex <38962279+alexvec@users.noreply.github.com>
2023-07-02 22:14:13 +0200

    Added support for domain scraping

--- pkg/platforms/hackerone/hackerone.go
+++ pkg/platforms/hackerone/hackerone.go
@@ -1,7 +1,9 @@
 package hackerone
 
 import (
+	"encoding/json"
 	"net/http"
+	"regexp"
 	"strconv"
 	"strings"
 	"sync"
@@ -19,8 +21,55 @@ const (
 	RATE_LIMIT_HTTP_STATUS   = 429
 )
 
+type Program struct {
+	ID         string `json:"id,omitempty"`
+	Type       string `json:"type,omitempty"`
+	Attributes struct {
+		Handle                          string    `json:"handle,omitempty"`
+		Name                            string    `json:"name,omitempty"`
+		Currency                        string    `json:"currency,omitempty"`
+		ProfilePicture                  string    `json:"profile_picture,omitempty"`
+		SubmissionState                 string    `json:"submission_state,omitempty"`
+		TriageActive                    any       `json:"triage_active,omitempty"`
+		State                           string    `json:"state,omitempty"`
+		StartedAcceptingAt              time.Time `json:"started_accepting_at,omitempty"`
+		NumberOfReportsForUser          int       `json:"number_of_reports_for_user,omitempty"`
+		NumberOfValidReportsForUser     int       `json:"number_of_valid_reports_for_user,omitempty"`
+		BountyEarnedForUser             float64   `json:"bounty_earned_for_user,omitempty"`
+		LastInvitationAcceptedAtForUser any       `json:"last_invitation_accepted_at_for_user,omitempty"`
+		Bookmarked                      bool      `json:"bookmarked,omitempty"`
+		AllowsBountySplitting           bool      `json:"allows_bounty_splitting,omitempty"`
+		OffersBounties                  bool      `json:"offers_bounties,omitempty"`
+	} `json:"attributes,omitempty"`
+	Relationships struct {
+		StructuredScopes struct {
+			Data []struct {
+				ID         string `json:"id,omitempty"`
+				Type       string `json:"type,omitempty"`
+				Attributes struct {
+					AssetType                  string    `json:"asset_type,omitempty"`
+					AssetIdentifier            string    `json:"asset_identifier,omitempty"`
+					EligibleForBounty          bool      `json:"eligible_for_bounty,omitempty"`
+					EligibleForSubmission      bool      `json:"eligible_for_submission,omitempty"`
+					Instruction                string    `json:"instruction,omitempty"`
+					MaxSeverity                string    `json:"max_severity,omitempty"`
+					Reference                  string    `json:"reference,omitempty"`
+					CreatedAt                  time.Time `json:"created_at,omitempty"`
+					UpdatedAt                  time.Time `json:"updated_at,omitempty"`
+					ConfidentialityRequirement string    `json:"confidentiality_requirement,omitempty"`
+					IntegrityRequirement       string    `json:"integrity_requirement,omitempty"`
+					AvailabilityRequirement    string    `json:"availability_requirement,omitempty"`
+				} `json:"attributes,omitempty"`
+			} `json:"data,omitempty"`
+		} `json:"structured_scopes,omitempty"`
+	} `json:"relationships,omitempty"`
+}
+
 func getProgramScope(authorization string, id string, bbpOnly bool, categories []string) (pData scope.ProgramData) {
-	var err error
+	r, err := regexp.Compile("(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]")
+	if err != nil {
+		utils.Log.Fatal("Could not create regex pattern for id ", id)
+	}
 	res := &whttp.WHTTPRes{}
 	lastStatus := -1
 
@@ -54,16 +103,26 @@ func getProgramScope(authorization string, id string, bbpOnly bool, categories [
 		utils.Log.Fatal("Could not retrieve data for id ", id, " with status ", lastStatus)
 	}
 
-	pData.Url = "https://hackerone.com/" + id
+	//pData.Url = "https://hackerone.com/" + id
+	pData.Url = id
 
-	l := int(gjson.Get(res.BodyString, "relationships.structured_scopes.data.#").Int())
+	var program Program
+
+	err = json.Unmarshal([]byte(res.BodyString), &program)
+
+	if err != nil {
+		utils.Log.Fatal("Could not parse program for id  ", id, " with status ", lastStatus)
+	}
+
+	l := len(program.Relationships.StructuredScopes.Data)
 
 	isDumpAll := len(categories) == len(getCategories("all"))
+	targets := make(map[string]struct{})
 	for i := 0; i < l; i++ {
 
 		catFound := false
 		if !isDumpAll {
-			assetCategory := gjson.Get(res.BodyString, "relationships.structured_scopes.data."+strconv.Itoa(i)+".attributes.asset_type").Str
+			assetCategory := program.Relationships.StructuredScopes.Data[i].Attributes.AssetType
 
 			for _, cat := range categories {
 				if cat == assetCategory {
@@ -75,27 +134,56 @@ func getProgramScope(authorization string, id string, bbpOnly bool, categories [
 
 		if catFound || isDumpAll {
 			// If it's in the in-scope table (and not in the OOS one)
-			if gjson.Get(res.BodyString, "relationships.structured_scopes.data."+strconv.Itoa(i)+".attributes.eligible_for_submission").Bool() {
-				if !bbpOnly || (bbpOnly && gjson.Get(res.BodyString, "relationships.structured_scopes.data."+strconv.Itoa(i)+".attributes.eligible_for_bounty").Bool()) {
-					pData.InScope = append(pData.InScope, scope.ScopeElement{
-						Target:      gjson.Get(res.BodyString, "relationships.structured_scopes.data."+strconv.Itoa(i)+".attributes.asset_identifier").Str,
-						Description: strings.ReplaceAll(gjson.Get(res.BodyString, "relationships.structured_scopes.data."+strconv.Itoa(i)+".attributes.instruction").Str, "\n", "  "),
-						Category:    "", // TODO
-					})
+			if program.Relationships.StructuredScopes.Data[i].Attributes.EligibleForSubmission {
+				if !bbpOnly || (bbpOnly && program.Relationships.StructuredScopes.Data[i].Attributes.EligibleForBounty) {
+					if program.Relationships.StructuredScopes.Data[i].Attributes.AssetType == "DOMAIN" || program.Relationships.StructuredScopes.Data[i].Attributes.AssetType == "URL" || program.Relationships.StructuredScopes.Data[i].Attributes.AssetType == "OTHER" || program.Relationships.StructuredScopes.Data[i].Attributes.AssetType == "WILDCARD" {
+						for _, match := range r.FindAllString(program.Relationships.StructuredScopes.Data[i].Attributes.AssetIdentifier, -1) {
+							_, ok := targets[match]
+							if !ok {
+								pData.InScope = append(pData.InScope, scope.ScopeElement{
+									Target:      match,
+									Description: strings.ReplaceAll(program.Relationships.StructuredScopes.Data[i].Attributes.Instruction, "\n", "  "),
+									Category:    "", // TODO
+								})
+								targets[match] = struct{}{}
+							}
+						}
+						for _, match := range r.FindAllString(program.Relationships.StructuredScopes.Data[i].Attributes.Instruction, -1) {
+							_, ok := targets[match]
+							if !ok {
+								pData.InScope = append(pData.InScope, scope.ScopeElement{
+									Target:      match,
+									Description: strings.ReplaceAll(program.Relationships.StructuredScopes.Data[i].Attributes.Instruction, "\n", "  "),
+									Category:    "", // TODO
+								})
+								targets[match] = struct{}{}
+							}
+						}
+					} else {
+						pData.InScope = append(pData.InScope, scope.ScopeElement{
+							Target:      program.Relationships.StructuredScopes.Data[i].Attributes.AssetIdentifier,
+							Description: strings.ReplaceAll(program.Relationships.StructuredScopes.Data[i].Attributes.Instruction, "\n", "  "),
+							Category:    "", // TODO
+						})
+					}
 				}
 			}
 		}
 	}
 
-	if l == 0 {
-		pData.InScope = append(pData.InScope, scope.ScopeElement{Target: "NO_IN_SCOPE_TABLE", Description: "", Category: ""})
-	}
+	/*
+		if l == 0 {
+			pData.InScope = append(pData.InScope, scope.ScopeElement{Target: "NO_IN_SCOPE_TABLE", Description: "", Category: ""})
+		}
+	*/
 
 	return pData
 }
 
 func getCategories(input string) []string {
 	categories := map[string][]string{
+		"domain":     {"DOMAIN"},
+		"wildcard":   {"WILDCARD"},
 		"url":        {"URL"},
 		"cidr":       {"CIDR"},
 		"mobile":     {"GOOGLE_PLAY_APP_ID", "OTHER_APK", "APPLE_STORE_APP_ID"},
@@ -105,7 +193,8 @@ func getCategories(input string) []string {
 		"hardware":   {"HARDWARE"},
 		"code":       {"SOURCE_CODE"},
 		"executable": {"DOWNLOADABLE_EXECUTABLES"},
-		"all":        {"URL", "CIDR", "GOOGLE_PLAY_APP_ID", "OTHER_APK", "APPLE_STORE_APP_ID", "OTHER", "HARDWARE", "SOURCE_CODE", "DOWNLOADABLE_EXECUTABLES"},
+		"all":        {"DOMAIN", "WILDCARD", "URL", "CIDR", "GOOGLE_PLAY_APP_ID", "OTHER_APK", "APPLE_STORE_APP_ID", "OTHER", "HARDWARE", "SOURCE_CODE", "DOWNLOADABLE_EXECUTABLES"},
+		"allinfra":   {"DOMAIN", "WILDCARD", "URL", "CIDR", "OTHER"},
 	}
 
 	selectedCategory, ok := categories[strings.ToLower(input)]
